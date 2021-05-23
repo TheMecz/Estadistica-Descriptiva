4b25f75f51cb709f995cd53d0aa362e4de0d567c
Max Albert Ponce Sanchez <55026548+TheMecz@users.noreply.github.com>
2021-05-22 21:35:53 -0500

    Fin del curso
    Subida de todos los archivos del curso master de machine learning

---
+++ scripts-max/04_calculadora.R
@@ -0,0 +1,91 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3 + 5
+
+2%%3
+
+2^3*5
+
+2^-4
+
+725%/%7
+
+725%%7
+
+# D = d * q + r
+# r = D - d * q
+# x = D %% d
+# x = D %% d
+
+pi
+
+2*pi
+
+3^pi
+
+pi^2
+
+Inf
+
+-Inf
+
+NA
+
+NaN
+
+5/0
+
+0/0
+
+2^50 # = 1.1259e+15 = 1125900000000000
+
+2^(-15) # = 3.051758e-05 = 0.00003051758
+
+c(2^30, 2^(-15), 1, 2, 3/2)
+
+sqrt(25)
+
+exp(1)
+
+log(pi)
+
+abs(-pi)
+
+factorial(7)
+
+factorial(4)
+
+factorial(5)
+
+choose(5, 4)
+
+cos(60*pi/180)
+
+sin(60*pi/180)
+
+sinpi(1/2) # = sin(pi/2)
+
+tan(pi) # 1.633124e-16 ~ 0
+
+tan(pi/2) # 1.633124e+16 ~ Inf
+
+asin(0.8660254) #arc sin en radianes
+
+asin(0.8660254)*180/pi #arc sin en grados
+
+#asin(5) #arc sin x in [-1,1]
+
+#acos(-8)
+
+print(sqrt(2), 10)
+round(sqrt(2),3)
+floor(sqrt(2))
+ceiling(sqrt(2))
+trunc(sqrt(2))
+
+
+
